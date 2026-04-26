ce1f24c0b12c8d4dac43579482eed37f10638d67
GFaceAce <raw23004@byui.edu>
2026-04-25 23:42:00 -0600

    Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Globalization;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        string letter = "";
+
+        if (number < 60)
+        {
+            letter = "F";
+        }
+        else if (number < 70)
+        {
+            letter = "D";
+        }
+        else if (number < 80)
+        {
+            letter = "C";
+        }
+        else if (number < 90)
+        {
+            letter = "B";
+        }
+        else
+        {
+            letter = "A";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed.");
+        }
     }
 }
